39efa59b4f6f22f5792b504089b57c4a85e48ce1
qandi <dltmdwo430@gmail.com>
2023-10-19 17:01:12 +0900

    create body to home_screin.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -19,6 +20,7 @@ class App extends StatelessWidget {
             ),
           ),
           cardColor: const Color(0xFFF4EDDB)),
+      home: const HomeScreen(),
     );
   }
 }
