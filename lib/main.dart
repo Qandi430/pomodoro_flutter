bf12f98b0ee569e6ea228b53c55f3509b55af68e
qandi <dltmdwo430@gmail.com>
2023-10-19 14:48:13 +0900

    create theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,15 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+          backgroundColor: const Color(0xFFE7626C),
+          textTheme: const TextTheme(
+            headline1: TextStyle(
+              color: Color(0xFF232B55),
+            ),
+          ),
+          cardColor: const Color(0xFFF4EDDB)),
+    );
   }
 }
